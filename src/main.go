9db9cae53968ef4d1806ef5239952a97c458a614
Tom <tsenseless@gmail.com>
2026-02-10 10:45:08 -0500

    Update

--- src/main.go
+++ src/main.go
@@ -22,12 +22,12 @@ const (
 	apiBase = "https://discord.com/api/v9"
 
 	// Conservative pacing to reduce transient 400/429 churn.
-	searchDelay          = 225 * time.Millisecond
-	deleteDelay          = 225 * time.Millisecond
-	reactionDelay        = 225 * time.Millisecond
-	batchDelay           = 225 * time.Millisecond
-	threadDiscoveryDelay = 225 * time.Millisecond
-	threadArchiveDelay   = 225 * time.Millisecond
+	searchDelay          = 235 * time.Millisecond
+	deleteDelay          = 235 * time.Millisecond
+	reactionDelay        = 235 * time.Millisecond
+	batchDelay           = 235 * time.Millisecond
+	threadDiscoveryDelay = 235 * time.Millisecond
+	threadArchiveDelay   = 235 * time.Millisecond
 	errorBackoffDelay    = 2000 * time.Millisecond
 
 	maxSearchIndexWaits = 40
